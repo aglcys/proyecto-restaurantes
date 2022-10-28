55ac49349dcf8a6f1a5352f7029e4b9ea34b27e6
Wendy Manzanero <wendypmg03@gmail.com>
2022-10-27 21:29:34 -0300

    agregue 2da parte del seed data

---
+++ seed_data.py
@@ -0,0 +1,8 @@
+from appfoodie.models import Restaurante
+
+Restaurante(nombre="Akiko", direccion="Blanco Encalada 2286",telefono=11212523, rango_precios="1000-5000",horario="11:00 a 23.30", tipo_comida="sushi").save()
+Restaurante(nombre="Hatsu", direccion="Cabildo 4569", telefono=11145698, rango_precios="2000-5000",horario="11:00 a 23.30", tipo_comida="sushi").save()
+Restaurante(nombre="Jiro Sushi", direccion="Rio Parana 745", telefono=11231245, rango_precios="1000-5000",horario="11:00 a 23.30", tipo_comida="sushi").save()
+Restaurante(nombre="Solomia", direccion="Quesada 856", telefono=11987523, rango_precios="1000-8000",horario="12:00 a 23.30", tipo_comida="parrilla").save()
+Restaurante(nombre="Praline", direccion="Monroe 1236", telefono=11147153, rango_precios="1000-5000",horario="9:00 a 20:00", tipo_comida="tortas").save()
+Restaurante(nombre="Tufic Helados", direccion="Juramento 1236",telefono=11456321, rango_precios="500-5000",horario="9:00 a 00:00", tipo_comida="helado").save()
